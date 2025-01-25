d59325c7e2ca96c17048f037348bee92c0352da1
Thomas J. Fan <thomasjpfan@gmail.com>
2025-01-25 13:12:55 -0500

    Try abi3

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, Extension
 from wheel.bdist_wheel import bdist_wheel
 import sys
+from Cython.Build import cythonize
 
 # Limited ABI is only available in 3.11
 use_abi3 = sys.version_info >= (3, 11)
@@ -28,7 +29,7 @@ class bdist_wheel_abi3(bdist_wheel):
 
 extensions = [
     Extension(
-        "adder.add",
+        "wordcloud.query_integral_image",
         sources=["wordcloud/query_integral_image.pyx"],
         py_limited_api=py_limited_api,
         define_macros=define_macros,
@@ -37,6 +38,6 @@ extensions = [
 
 
 setup(
-    ext_modules=extensions,
+    ext_modules=cythonize(extensions),
     cmdclass={"bdist_wheel": bdist_wheel_abi3},
 )
