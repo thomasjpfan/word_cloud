f981ae41ca528488686aed45fb6d8101f3a0e723
Thomas J. Fan <thomasjpfan@gmail.com>
2025-12-21 21:22:28 -0500

    Try abi3

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@ from Cython.Build import cythonize
 import sys
 
 extension_kwargs = {}
+options = {}
 
-if sys.version_info >= (3, 11):
+is_gil_enabled = hasattr(sys, "_is_gil_enabled") and sys._is_gil_enabled()
+
+if sys.version_info >= (3, 11) and not is_gil_enabled:
     extension_kwargs["define_macros"] = [
         ("Py_LIMITED_API", 0x030B0000)
     ]
     extension_kwargs["py_limited_api"] = True
+    options["bdist_wheel"] = {"py_limited_api": "cp311"}
 
 
 setup(ext_modules=cythonize(
@@ -18,5 +22,6 @@ setup(ext_modules=cythonize(
             sources=["wordcloud/query_integral_image.pyx"],
             **extension_kwargs
         )
-    ]
+    ],
+    options=options
 ))
