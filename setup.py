8ec7cf00980586531382e81ece0aeef89d40e340
Thomas J. Fan <thomasjpfan@gmail.com>
2025-12-21 21:07:23 -0500

    Use limited abi for 3.11+

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
-from setuptools import setup
+from setuptools import setup, Extension
 from Cython.Build import cythonize
+import sys
 
-setup(ext_modules=cythonize("wordcloud/query_integral_image.pyx"))
+extension_kwargs = {}
+
+if sys.version_info >= (3, 11):
+    extension_kwargs["define_macros"] = [
+        ("Py_LIMITED_API", 0x030B0000)
+    ]
+    extension_kwargs["py_limited_api"] = True
+
+
+setup(ext_modules=cythonize(
+    [
+        Extension(
+            name="wordcloud.query_integral_image",
+            sources=["wordcloud/query_integral_image.pyx"],
+            **extension_kwargs
+        )
+    ]
+))
